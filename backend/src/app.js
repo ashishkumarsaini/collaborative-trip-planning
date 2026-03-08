57f44a3d9aa56e63e91bc7d5c36f3b2bd419f695
ashishkumarsaini <develop.ashishsaini@gmail.com>
2026-03-08 16:16:33 +0530

    adding cors origin

--- backend/src/app.js
+++ backend/src/app.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: true, limit: '16kb' }));
 app.use(express.static('public'));
 app.use(cookieParser());
 app.use(cors({
-    origin: "http://localhost:3000", // your frontend
+    origin: process.env.CORS_ORIGIN,
     credentials: true
 }));
 
